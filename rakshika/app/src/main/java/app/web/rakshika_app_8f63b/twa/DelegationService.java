170f63fa0ec39bbe654658e336f80fc7308647fe
Rakshika <288649685+rakshikahq@users.noreply.github.com>
2026-05-29 15:41:10 +0000

    u

---
+++ rakshika/app/src/main/java/app/web/rakshika_app_8f63b/twa/DelegationService.java
@@ -0,0 +1,18 @@
+package app.web.rakshika_app_8f63b.twa;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
